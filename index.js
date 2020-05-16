596e42b70831586864cabf4d0f9f7495f416b637
Thomas Baxendale <tom.baxendale@innovise.com>
2020-05-16 01:58:43 +0100

    fixed error in get request binding

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.get('./', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
