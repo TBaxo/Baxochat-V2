2fa5cc136a9a5756f0ab001499242e83486aeadf
Thomas Baxendale <tom.baxendale@innovise.com>
2020-05-16 23:37:07 +0100

    now keeps track of users who are online and also prints usernames next to messages

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
 //index page
+const express = require('express');
+const bodyparser = require('body-parser');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const connected_users = {};
+
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-//app.get('/chat')
+app.post('/', function (req, res) {
+    var endpoint = `/chat?username=${req.body.username}`
+    res.redirect(endpoint);
+});
+
+app.get('/chat', (req, res) => {
+    res.sendFile(__dirname + '/chat.html');
+});
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    let username = socket.handshake.query.username;
+    console.log(`${username} has connected`);
 
-    socket.on("chat_message", (msg) => {
-        io.emit("chat_message", msg);
-    });
+    if (!connected_users[socket.id]) {
 
-    socket.broadcast.emit("chat_message", "User has joined the chat");
+        socket.on("chat_message", (msg) => {
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+            io.emit("chat_message", msg);
+        });
+
+        socket.broadcast.emit("user_join", `${username} has joined the chat`);
+
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+
+            delete connected_users[socket.id];
+        });
+
+        connected_users[socket.id] = username;
+    }
 });
 
 http.listen(8080, () => {
