c9aa858eb1b14ec3e4cb1cf222dc447f72196cde
Thomas Baxendale <tombaxendale95@gmail.com>
2021-10-13 01:06:56 +0100

    removed model base

--- shared/Models/IModelBase.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=IModelBase.js.map
--- shared/Models/User/User.ts
+++ shared/Models/User/User.ts
@@ -6,7 +6,6 @@ export class User {
     public username: string;
 
 
-
     constructor(username: string) {
         this.username = username;
     }
